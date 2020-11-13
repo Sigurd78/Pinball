f8c561d3b68b4e0b200c217a4cddb6c54cfb29c5
Nathan Midgett <natemidgett@gmail.com>
2020-11-13 16:23:50 -0500

    Added code
    Made code for the ball, the point values of the bumpers and tunnels, and the high score. Still need a way to launch the ball, a limited # of balls, destruction of the ball, and a spawn point for the ball. Also I want to make the bumpers light up when the ball hits them. Might borrow from HW #2 and HW #3 a bit. I think I can add one last component at the very top of the table, past the two arches. Maybe highest point value goal? Also, I still need to figure out where to put Bill. Maybe if you get a certain point score, he rises up behind the table and just stares at the player. That'd be interesting and creepy at the same time. I'll keep it in mind.

---
+++ Pinball/Assets/Scripts/Ball.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+public class Ball : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Bumper")
+        {
+            Destroy(collidedWith);
+
+            int score = int.Parse(scoreGT.text);
+            score += 50;
+            scoreGT.text = score.ToString();
+
+            if (score > HighScore.score)
+            {
+                HighScore.score = score;
+            }
+        }
+
+        if (collidedWith.tag == "Tunnel")
+        {
+            Destroy(collidedWith);
+
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
+
+            if (score > HighScore.score)
+            {
+                HighScore.score = score;
+            }
+        }
+
+    }
+}
